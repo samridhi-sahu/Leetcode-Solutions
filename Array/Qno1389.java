c6bf2d561d998bc97b42de3b16527cc3ceed219c
Samridhi Sahu <98205847+samridhi-sahu@users.noreply.github.com>
2023-02-18 18:05:34 +0530

    added

---
+++ Array/Qno1389.java
@@ -0,0 +1,36 @@
+// 1389. Create Target Array in the Given Order
+
+import java.util.*;
+
+public class Qno1389 {
+
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> result = new LinkedList<Integer>();
+        int x, y;
+        int[] target = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            x = index[i];
+            y = nums[i];
+            result.add(x, y);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            target[i] = result.get(i);
+        }
+        System.out.print(Arrays.toString(target));
+        return target;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+        int[] index = new int[size];
+        for (int i = 0; i < size; i++) {
+            nums[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            index[i] = sc.nextInt();
+        }
+        int[] res = createTargetArray(nums, index);
+    }
+}
