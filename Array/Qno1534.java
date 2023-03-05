030c2fd0b905f5c5f5eeca4a4617857239b977a6
Samridhi Sahu <98205847+samridhi-sahu@users.noreply.github.com>
2023-03-05 21:46:14 +0530

    added

---
+++ Array/Qno1534.java
@@ -0,0 +1,30 @@
+// 1534. Count Good Triplets
+public class Qno1534 {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int l = arr.length;
+        int x, y, z;
+        int count = 0;
+        for (int i = 0; i < l - 2; i++) {
+            for (int j = i + 1; j < l - 1; j++) {
+                for (int k = j + 1; k < l; k++) {
+                    x = arr[i] - arr[j];
+                    if (x < 0) {
+                        x *= -1;
+                    }
+                    y = arr[j] - arr[k];
+                    if (y < 0) {
+                        y *= -1;
+                    }
+                    z = arr[i] - arr[k];
+                    if (z < 0) {
+                        z *= -1;
+                    }
+                    if (i < j && j < k && x <= a && y <= b && z <= c) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
