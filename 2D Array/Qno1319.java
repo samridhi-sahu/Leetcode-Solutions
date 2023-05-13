e179b1c76c8dd6f588a14e79d7dec59da2dfd7f5
Samridhi Sahu <98205847+samridhi-sahu@users.noreply.github.com>
2023-05-13 23:15:20 +0530

    added

---
+++ 2D Array/Qno1319.java
@@ -0,0 +1,20 @@
+// 2319. Check if Matrix Is X-Matrix
+// https://leetcode.com/problems/check-if-matrix-is-x-matrix/description/
+
+public class Qno1319 {
+    public boolean checkXMatrix(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+        for (int i = 0, k = n - 1; i < n && k >= 0; i++, k--) {
+            if (i != k && mat[i][k] == 0)
+                return false;
+            for (int j = 0; j < n; j++) {
+                if (i == j && mat[i][j] == 0)
+                    return false;
+                if (i != j && j != k && mat[i][j] != 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ 2D Array/Qno2133.java
@@ -0,0 +1,28 @@
+// 2133. Check if Every Row and Column Contains All Numbers
+// https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/description/
+
+public class Qno2133 {
+    public boolean checkValid(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (map.containsKey(matrix[j][i])) {
+                    return false;
+                } else {
+                    map.put(matrix[j][i], 1);
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (map.containsKey(matrix[i][j])) {
+                    return false;
+                } else {
+                    map.put(matrix[i][j], 1);
+                }
+            }
+        }
+        return true;
+    }
+}
