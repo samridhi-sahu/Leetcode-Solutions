7af763dd42cf5c49d43bfc892e4d49de31b8bf8a
Samridhi Sahu <98205847+samridhi-sahu@users.noreply.github.com>
2023-07-22 00:41:38 +0530

    added

---
+++ Array/RemoveDuplicateQ80.java
@@ -0,0 +1,25 @@
+// 80. Remove Duplicates from Sorted Array II
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+public class RemoveDuplicateQ80 {
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+        int flag = 0;
+        int i = 0;
+        while (i < nums.length - 1) {
+            if (nums[i] == nums[i + 1])
+                flag = 1;
+            else {
+                nums[k++] = nums[i];
+                if (flag == 1) {
+                    nums[k++] = nums[i];
+                    flag = 0;
+                }
+            }
+            i++;
+        }
+        nums[k++] = nums[i];
+        if (flag == 1)
+            nums[k++] = nums[i];
+        return k;
+    }
+}
